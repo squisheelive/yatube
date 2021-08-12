99aa3d8b0488477ffe3d214cd47f3e6cfc2a15af
Vladimir Smirnov <squisheelive@gmail.com>
2021-08-12 13:05:48 +0300

    sprint-6, HW-6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -30,7 +30,6 @@ def group_posts(request, slug):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     post_list = author.posts.all()
-    post = author.posts.first
     following = False
     if request.user.is_anonymous is True:
         pass
@@ -42,7 +41,6 @@ def profile(request, username):
     context = {
         'page': page,
         'author': author,
-        'post': post,
         'following': following,
     }
     return render(request, 'profile.html', context)
